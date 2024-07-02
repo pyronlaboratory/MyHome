14bc2940a76725fa48173c732ce7372819da9fe3
Ronnie <ronnie94official@gmail.com>
2024-07-02 15:02:58 +0100

    Update AuthenticationException.java+++

--- service/src/main/java/com/myhome/controllers/exceptions/AuthenticationException.java
+++ service/src/main/java/com/myhome/controllers/exceptions/AuthenticationException.java
@@ -4,13 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-/**
- * is an extension of RuntimeException and has a default error message "Credentials
- * are incorrect or user does not exist".
- * Fields:
- * 	- ERROR_MESSAGE (String): is a string constant representing an error message
- * related to authentication credentials being incorrect or a user not existing.
- */
 @Slf4j
 @ResponseStatus(value = HttpStatus.UNAUTHORIZED)
 public class AuthenticationException extends RuntimeException {
