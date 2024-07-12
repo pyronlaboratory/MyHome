85e378901bc289f10356894afee8acec3baee91f
Ronnie <ronnie94official@gmail.com>
2024-07-12 19:41:34 +0100

    Update EmailTemplateProperties.java+++

--- service/src/main/java/com/myhome/configuration/properties/mail/EmailTemplateProperties.java
+++ service/src/main/java/com/myhome/configuration/properties/mail/EmailTemplateProperties.java
@@ -1,6 +1,4 @@
 package com.myhome.configuration.properties.mail;
-
-
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
